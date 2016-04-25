01ea167f9f7e3669a67bb5d1f749002fd627f8c5
Baptiste Masset <baptistemasset@gmail.com>
2016-04-25 19:02:13 +0200

    modif hillclimbing info

--- hillclimb/__init__.py
+++ hillclimb/__init__.py
@@ -1,4 +1,4 @@
-# Implementation of hillclimbing algorithm with restart variant + tests on tsp
+# Implementation of hillclimbing algorithm with restart variant
 # hillclimb(init_function,move_operator,objective_function,max_evaluations)
 # where init_function() – the function used to create our initial solution (random ?)
 #       move_operator() – the function we use to iterate over all possible “moves” for a given solution
